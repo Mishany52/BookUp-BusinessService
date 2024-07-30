9f7f623c32d4f7704f0fec67ece23db587730c9f
PavelBack <pavel1201s@yandex.ru>
2024-07-30 20:56:57 +0300

    fix(typeorm): fix config

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -9,7 +9,7 @@ config({ path: '.env' });
 const AppDataSource = new DataSource({
     type: 'postgres',
     host: process.env.POSTGRES_HOST,
-    port: parseInt(process.env.PORT),
+    port: parseInt(process.env.POSTGRES_PORT),
     username: `${process.env.POSTGRES_USER}`,
     password: `${process.env.POSTGRES_PASSWORD}`,
     database: `${process.env.POSTGRES_DB}`,
