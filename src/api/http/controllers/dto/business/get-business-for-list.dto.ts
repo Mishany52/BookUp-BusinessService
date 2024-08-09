831ab68bdb430b21f98bbe4d684da8549a926716
Mishany52 <kalintsev309206@gmail.com>
2024-08-09 19:59:47 +0300

    9-feat/get-list-business -fix mr commit -https://tracker.yandex.ru/BOOKUPDEV-9

--- src/api/http/controllers/dto/business/get-business-for-list.dto.ts
+++ src/api/http/controllers/dto/business/get-business-for-list.dto.ts
@@ -6,21 +6,27 @@ export class GetBusinessCountAllWorkersDto {
     @ApiProperty()
     @IsNumber()
     id: number;
+
     @ApiProperty()
     @IsString()
     name: string;
+
     @ApiProperty()
     @IsString()
     logoUrl: string;
+
     @ApiProperty()
     @IsNumber()
     pointCount: number;
+
     @ApiProperty()
     @IsNumber()
     adminCount: number;
+
     @ApiProperty()
     @IsNumber()
     managerCount: number;
+
     constructor(props: IBusinessProps) {
         this.id = props.id;
         this.name = props.name;
